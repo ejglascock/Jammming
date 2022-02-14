52d8f41376a577b32822082583a717b507d4cdfb
Elijah Glascock <ejglascock@gmail.com>
2022-02-14 13:22:40 -0800

    steps 48-55, added functionality to minus button, which removes tracks from the playlist.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
       playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4}, {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5}, {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -23,6 +24,12 @@ class App extends React.Component {
     }
 
     tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
     this.setState({playlistTracks: tracks});
   }
 
@@ -36,7 +43,8 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults}
                            onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
-                      playlistTracks={this.state.playlistTracks} />
+                      playlistTracks={this.state.playlistTracks} 
+                      onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
